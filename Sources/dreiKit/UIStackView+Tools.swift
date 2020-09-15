d3f68bd6a9fed63e45b8978f3cc27142c4eb7837
Nils Becker <nils.becker@dreipol.ch>
2020-09-15 14:48:18 +0200

    Revert "[srk] Fix addSpace not working at end of view"
    This reverts commit 570a485f4bb32dd9a2b840b6ad7b4158cbc0f620.

--- Sources/dreiKit/UIStackView+Tools.swift
+++ Sources/dreiKit/UIStackView+Tools.swift
@@ -18,11 +18,16 @@ public extension UIStackView {
     }
 
     func addSpace(_ height: CGFloat) {
-        let view = UIView()
-        view.isUserInteractionEnabled = false
-        let anchor = self.axis == .horizontal ? view.widthAnchor : view.heightAnchor
-        anchor.constraint(equalToConstant: height).isActive = true
-        addArrangedSubview(view)
+        // Seams fixed for now with iOS 13.2 SDK
+        if let view = arrangedSubviews.last {
+            setCustomSpacing(height, after: view)
+        } else {
+            let view = UIView()
+            view.isUserInteractionEnabled = false
+            let anchor = self.axis == .horizontal ? view.widthAnchor : view.heightAnchor
+            anchor.constraint(equalToConstant: height).isActive = true
+            addArrangedSubview(view)
+        }
     }
 
     func removeArrangedView(_ view: UIView) {
