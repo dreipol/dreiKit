f7824b04ed67a40d38625dec1af69c5c50b9afcc
Nils Becker <nils.becker@dreipol.ch>
2020-10-14 08:07:47 +0200

    [street-navigation] Add AddressFormatter

---
+++ Sources/dreiKit/AddressFormatter.swift
@@ -0,0 +1,29 @@
+//
+//  AddressFormatter.swift
+//  Transport
+//
+//  Created by Nils Becker on 14.10.20.
+//  Copyright © 2020 dreipol GmbH. All rights reserved.
+//
+
+import Foundation
+
+public struct Address {
+    let name: String?
+    let street: String?
+    let zip: String?
+    let city: String?
+    let country: String?
+}
+
+public final class AddressFormatter {
+    public init() {}
+
+    public func string(forDisplaying address: Address) -> String {
+        "\(address.name ?? ""), \(address.street ?? "")\n\(address.zip ?? "") \(address.city ?? "")"
+    }
+
+    public func string(forSearch address: Address) -> String {
+        "\(address.street ?? ""), \(address.zip ?? "") \(address.city ?? ""), \(address.country ?? "")"
+    }
+}
