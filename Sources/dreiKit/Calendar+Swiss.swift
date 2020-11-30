387815209e515e88644467b8cbb9cbe03c7c773f
Nils Becker <nils.becker@dreipol.ch>
2020-11-30 08:10:00 +0100

    [calendar-helpers] Add default calendars

---
+++ Sources/dreiKit/Calendar+Swiss.swift
@@ -0,0 +1,42 @@
+//
+//  Calendar+Swiss.swift
+//  dreiKit
+//
+//  Created by Nils Becker on 27.11.20.
+//
+
+import Foundation
+
+public extension Calendar {
+    static func swissGerman() -> Calendar {
+        var calendar = Calendar(identifier: .gregorian)
+        // swiftlint:disable:next force_unwrapping
+        calendar.timeZone = TimeZone(identifier: "CET")!
+        calendar.locale = Locale(identifier: "gsw_CH")
+        return calendar
+    }
+
+    static func swissFrench() -> Calendar {
+        var calendar = Calendar(identifier: .gregorian)
+        // swiftlint:disable:next force_unwrapping
+        calendar.timeZone = TimeZone(identifier: "CET")!
+        calendar.locale = Locale(identifier: "fr_CH")
+        return calendar
+    }
+
+    static func swissItalian() -> Calendar {
+        var calendar = Calendar(identifier: .gregorian)
+        // swiftlint:disable:next force_unwrapping
+        calendar.timeZone = TimeZone(identifier: "CET")!
+        calendar.locale = Locale(identifier: "it_CH")
+        return calendar
+    }
+
+    static func swissRomansh() -> Calendar {
+        var calendar = Calendar(identifier: .gregorian)
+        // swiftlint:disable:next force_unwrapping
+        calendar.timeZone = TimeZone(identifier: "CET")!
+        calendar.locale = Locale(identifier: "rm_CH")
+        return calendar
+    }
+}
