08359d4ab682b0a82d0b6615b595d8446150ed1b
Nils Becker <nils.becker@dreipol.ch>
2021-05-06 15:22:36 +0200

    [voice_over_helpers] Add global default accessibility language

---
+++ Sources/dreiKit/Accessibility/VoiceOver/UIView+defaultVOLanguage.swift
@@ -0,0 +1,48 @@
+//
+//  UIView+defaultVOLanguage.swift
+//  dreiKit
+//
+//  Created by Nils Becker on 06.05.21.
+//
+
+import UIKit
+
+private enum AssociatedObjectKey {
+    case overrideAccessibiltyLanguage
+
+    static var overrideAccessibiltyLanguageKey = AssociatedObjectKey.overrideAccessibiltyLanguage
+}
+
+public extension UIView {
+    static var defaultAccessibilityLanguage: String! {
+        didSet {
+            assert(swizzled, "Failed to swizzle accessibility language")
+        }
+    }
+
+    // implicitly lazy => dispatch_once
+    private static var swizzled: Bool = {
+        guard let defaultingMethod = class_getInstanceMethod(UIView.self, #selector(swizzled_accessibilityLanguage)) else {
+            return false
+        }
+        let implementation = method_getImplementation(defaultingMethod)
+        let types = method_getTypeEncoding(defaultingMethod)
+        guard let overrideImpl = class_replaceMethod(UIView.self, #selector(accessibilityLanguage), implementation, types) else {
+            return false
+        }
+        _ = class_replaceMethod(UIView.self, #selector(overrideAccessibiltyLanguage), overrideImpl, types)
+        return true
+    }()
+
+    @objc private func swizzled_accessibilityLanguage() -> String? {
+        if let overrideAccessibilityLanguage = overrideAccessibiltyLanguage() {
+            return overrideAccessibilityLanguage
+        }
+
+        return Self.defaultAccessibilityLanguage
+    }
+
+    @objc private func overrideAccessibiltyLanguage() -> String? {
+        nil
+    }
+}
