66d4ec7c49ed9e30d6e1b7c27503860601b7de7c
Samuel Bichsel <samuel.bichsel@dreipol.ch>
2023-07-18 13:34:03 +0200

    [colors] Rename to clearity

--- Sources/dreiKit/SwiftUI/Color+Extensions.swift
+++ Sources/dreiKit/SwiftUI/Color+Extensions.swift
@@ -14,11 +14,11 @@ import UIKit
 private let nonHexCharacters: CharacterSet = .whitespacesAndNewlines.union(CharacterSet(charactersIn: "#"))
 
 public extension Color {
-    init(_ rgb: Int) {
-        let r = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
-        let g = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
-        let b = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
-        let a = CGFloat(rgb & 0x000000FF) / 255.0
+    init(_ rgba: Int) {
+        let r = CGFloat((rgba & 0xFF000000) >> 24) / 255.0
+        let g = CGFloat((rgba & 0x00FF0000) >> 16) / 255.0
+        let b = CGFloat((rgba & 0x0000FF00) >> 8) / 255.0
+        let a = CGFloat(rgba & 0x000000FF) / 255.0
         self.init(red: r, green: g, blue: b, opacity: a)
     }
 
