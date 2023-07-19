6eac346acddfe688fdbdb542e4643a4f28383c98
Samuel Bichsel <samuel.bichsel@dreipol.ch>
2023-07-19 19:20:05 +0200

    Add kerning and fontColor

--- Sources/dreiKit/SwiftUI/TextFormat.swift
+++ Sources/dreiKit/SwiftUI/TextFormat.swift
@@ -16,21 +16,37 @@ public extension Text {
     }
 }
 
+private extension View {
+    @ViewBuilder func foregroundColorIfSet(_ color: Color?) -> some View {
+        if let c = color {
+            self.foregroundColor(c)
+        } else {
+            self
+        }
+    }
+}
+
 public struct TextFormat: TextModifier, Equatable {
     let font: Font
     let lineSpacing: CGFloat
     let tracking: CGFloat
-    
-    public init(font: Font, lineSpacing: CGFloat, tracking: CGFloat) {
+    let kerning: CGFloat
+    let color: Color?
+
+    public init(font: Font, lineSpacing: CGFloat = 0, tracking: CGFloat = 0, kerning: CGFloat = 0, color: Color? = nil) {
         self.font = font
         self.lineSpacing = lineSpacing
         self.tracking = tracking
+        self.kerning = kerning
+        self.color = color
     }
-    
+
     public func body(_ view: Text) -> some View {
         return view
             .font(font)
             .tracking(tracking)
+            .kerning(kerning)
             .lineSpacing(lineSpacing)
+            .foregroundColorIfSet(color)
     }
 }
