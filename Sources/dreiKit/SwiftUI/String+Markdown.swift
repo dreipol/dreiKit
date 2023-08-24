82067ee82efe41482dd011e9b55241a87bd2dc1b
Samuel Bichsel <samuel.bichsel@dreipol.ch>
2023-08-24 11:02:14 +0200

    [swiftui-html] Support strong type

--- Sources/dreiKit/SwiftUI/String+Markdown.swift
+++ Sources/dreiKit/SwiftUI/String+Markdown.swift
@@ -9,11 +9,13 @@ import Foundation
 import SwiftUI
 
 public extension String {
-    func convertHTMLLinksToMarkdown() -> LocalizedStringKey {
+    /// Convert html tags to markdown so that they can be used in SwiftUI
+    /// - Important: So long only **links and bold** is supported.
+    func convertHTMLTagsMarkdown() -> LocalizedStringKey {
         var markdownString = self
 
-        let regexPattern = #"<a\s+href="([^"]+)"[^>]*>(.*?)</a>"#
-//                // swiftlint:disable:next force_try
+        let regexPattern = #"<a\s+href="([^"]+)"[^>]*>(.*?)</a>|<strong>(.*?)</strong>"#
+        // swiftlint:disable:next force_try
         let regex = try! NSRegularExpression(pattern: regexPattern, options: [])
 
         let matches = regex.matches(in: markdownString, options: [], range: NSRange(location: 0, length: markdownString.utf16.count))
@@ -21,6 +23,7 @@ public extension String {
         for match in matches.reversed() {
             let hrefRange = match.range(at: 1)
             let textRange = match.range(at: 2)
+            let strongRange = match.range(at: 3)
 
             if hrefRange.location != NSNotFound && textRange.location != NSNotFound,
                let href = Range(hrefRange, in: markdownString),
@@ -29,8 +32,25 @@ public extension String {
                 let markdownLink = "[\(String(markdownString[text]))](\(String(markdownString[href])))"
                 markdownString.replaceSubrange(replaceRange, with: markdownLink)
             }
+
+            if strongRange.location != NSNotFound, let strong = Range(strongRange, in: markdownString) {
+                let markdownStrong = "**\(String(markdownString[strong]))**"
+                markdownString.replaceSubrange(Range(match.range, in: markdownString)!, with: markdownStrong)
+            }
         }
 
         return LocalizedStringKey(markdownString)
+    }
+}
+
+struct Previews_String_Markdown_Previews: PreviewProvider {
+    static let linkText = """
+                Für Informationen zur Herkunftsdeklaration von Produkten von A' Amico Pizza, <a href="https://uploads-ssl.webflow.com/ 60252f9c2d84b60986d1ca50/6321b81def89be Ob7bf303b4_A%60Amico%20Pizza_Fleischdek laration.pdf" target="_blank" rel="noreferrer"> klicke hier</a>.
+    """
+    static var previews: some View {
+        VStack {
+            Text(linkText.convertHTMLToMarkdown())
+            Text("<strong>Plane jetzt deine Bestellung.</strong> Du kannst für eine Lieferzeit <strong>ab 17:30 Uhr</strong> vorbestellen.".convertHTMLToMarkdown())
+        }
     }
 }
