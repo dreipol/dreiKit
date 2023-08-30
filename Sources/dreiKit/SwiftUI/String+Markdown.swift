48915687e9dc05609091b6809b08d5f9d64cb1ee
Samuel Bichsel <samuel.bichsel@dreipol.ch>
2023-08-30 10:44:20 +0200

    [attributed-markdown] Support Attributed string when converting html

--- Sources/dreiKit/SwiftUI/String+Markdown.swift
+++ Sources/dreiKit/SwiftUI/String+Markdown.swift
@@ -9,9 +9,7 @@ import Foundation
 import SwiftUI
 
 public extension String {
-    /// Convert html tags to markdown so that they can be used in SwiftUI
-    /// - Important: So long only **links and bold** is supported.
-    func convertHTMLTagsMarkdown() -> LocalizedStringKey {
+    private func convertToMarkdown() -> String {
         var markdownString = self
 
         let regexPattern = #"<a\s+href="([^"]+)"[^>]*>(.*?)</a>|<strong>(.*?)</strong>"#
@@ -38,11 +36,23 @@ public extension String {
                 markdownString.replaceSubrange(Range(match.range, in: markdownString)!, with: markdownStrong)
             }
         }
+        return markdownString
+    }
 
-        return LocalizedStringKey(markdownString)
+    /// Convert html tags to markdown so that they can be used in SwiftUI
+    /// - Important: So long only **links and bold** is supported.
+    func convertHTMLTagsMarkdown() -> LocalizedStringKey {
+        return LocalizedStringKey(convertToMarkdown())
+    }
+
+    /// Convert html tags to markdown so that they can be used as AttributedString
+    /// - Important: So long only **links and bold** is supported.
+    func convertHTMLTagsMarkdownAttributed() -> AttributedString? {
+        return try? AttributedString(markdown: convertToMarkdown())
     }
 }
 
+// swiftlint:disable all
 struct Previews_String_Markdown_Previews: PreviewProvider {
     static let linkText = """
                 Für Informationen zur Herkunftsdeklaration von Produkten von A' Amico Pizza, <a href="https://uploads-ssl.webflow.com/ 60252f9c2d84b60986d1ca50/6321b81def89be Ob7bf303b4_A%60Amico%20Pizza_Fleischdek laration.pdf" target="_blank" rel="noreferrer"> klicke hier</a>.
@@ -54,3 +64,4 @@ struct Previews_String_Markdown_Previews: PreviewProvider {
         }
     }
 }
+// swiftlint:enable all
