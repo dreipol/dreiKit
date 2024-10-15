b99f609bdf9da4ea8e860a74cac5c94aabec12c1
Samuel Bichsel <samuel.bichsel@dreipol.ch>
2024-10-15 14:58:47 +0200

    Add array helper

---
+++ Sources/dreiKit/DevWorkflow/Array+filterDuplicates.swift
@@ -0,0 +1,16 @@
+//
+//  Array+filterDuplicates.swift
+//  dreiKit
+//
+//  Created by Samuel Bichsel on 15.10.2024.
+//
+
+
+extension Array {
+    func filterDuplicates<T: Hashable>(_ key: (Element) -> T) -> [Element] {
+        var seen = [T: Bool]()
+        return filter {
+            seen.updateValue(true, forKey: key($0)) == nil
+        }
+    }
+}
