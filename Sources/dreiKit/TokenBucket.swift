8eed9ce1c5007cc0eb66e8fd21f7afaaca865a1d
Laila Becker <laila.becker@dreipol.ch>
2025-09-25 15:17:25 +0200

    [swift-6] Add token bucket

---
+++ Sources/dreiKit/TokenBucket.swift
@@ -0,0 +1,50 @@
+import Foundation
+
+public actor TokenBucket {
+    enum State {
+        case full
+        case empty(waiters: [CheckedContinuation<Void, Never>])
+    }
+
+    private var state: State
+
+    init() {
+        self.state = .full
+    }
+
+    private func enter() async {
+        switch self.state {
+        case .full:
+            self.state = .empty(waiters: [])
+            return
+        case .empty(waiters: var waiters):
+            await withCheckedContinuation {
+                waiters.append($0)
+                self.state = .empty(waiters: waiters)
+            }
+        }
+    }
+
+    private func exit() {
+        guard case .empty(waiters: var waiters) = self.state else {
+            fatalError("Exiting in invalid state")
+        }
+
+        if waiters.isEmpty {
+            self.state = .full
+            return
+        }
+
+        let nextWaiter = waiters.removeFirst()
+        self.state = .empty(waiters: waiters)
+        nextWaiter.resume()
+    }
+
+    func withToken<ReturnValue>(_ body: () async throws -> ReturnValue) async rethrows -> ReturnValue {
+        await self.enter()
+        defer {
+            self.exit()
+        }
+        return try await body()
+    }
+}
