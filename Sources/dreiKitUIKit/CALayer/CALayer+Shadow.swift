ffc8768f402e847fcaa5d8075334cc760ee69ba7
Laila Becker <laila.becker@dreipol.ch>
2025-09-30 15:31:44 +0200

    [swift-6] Fix indentation

--- Sources/dreiKitUIKit/CALayer/CALayer+Shadow.swift
+++ Sources/dreiKitUIKit/CALayer/CALayer+Shadow.swift
@@ -10,24 +10,24 @@ import UIKit
 public extension CALayer {
     @MainActor
     func addShadow(
-    color: UIColor,
-    x: CGFloat,
-    y: CGFloat,
-    blur: CGFloat,
-    spread: CGFloat) {
-        masksToBounds = false
-        shadowColor = color.cgColor
-        shadowOpacity = 1
-        shadowOffset = CGSize(width: x, height: y)
-        shadowRadius = blur / 2.0
-        if spread == 0 {
-          shadowPath = nil
-        } else {
-          let dx = -spread
-          let rect = bounds.insetBy(dx: dx, dy: dx)
-          shadowPath = UIBezierPath(rect: rect).cgPath
+        color: UIColor,
+        x: CGFloat,
+        y: CGFloat,
+        blur: CGFloat,
+        spread: CGFloat) {
+            masksToBounds = false
+            shadowColor = color.cgColor
+            shadowOpacity = 1
+            shadowOffset = CGSize(width: x, height: y)
+            shadowRadius = blur / 2.0
+            if spread == 0 {
+                shadowPath = nil
+            } else {
+                let dx = -spread
+                let rect = bounds.insetBy(dx: dx, dy: dx)
+                shadowPath = UIBezierPath(rect: rect).cgPath
+            }
+            shouldRasterize = true
+            rasterizationScale = UIScreen.main.scale
         }
-        shouldRasterize = true
-        rasterizationScale = UIScreen.main.scale
-  }
 }
